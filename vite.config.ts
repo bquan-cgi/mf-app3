5020c6cb36ed21ff5c64c10387d4e84175a6e604
bquan-cgi <bo.quan@cgi.com>
2024-06-11 16:11:03 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
         "./App3Home": "./src/App3Home"
       },
       remotes: {
-        shellApp: "http://localhost:5001/assets/remoteEntry.js",
-        //shellApp: "http://localhost:8080/assets/remoteEntry.js",
+        shellApp: "http://localhost:5001/assets/remoteEntry.js"
       },
       shared: ["react", "react-dom", "@group-ui/group-ui-react"],
     }),
